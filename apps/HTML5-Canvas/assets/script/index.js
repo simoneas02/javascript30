fa5e3bfb201c5c1cf98297bbc5e52a19882bb9db
simoneas02 <simoneas02@hotmail.com>
2019-06-08 22:01:47 -0300

    refactored the js file

--- apps/HTML5-Canvas/assets/script/index.js
+++ apps/HTML5-Canvas/assets/script/index.js
@@ -1,26 +1,22 @@
-const getElement = element => document.querySelector(element);
-const canvas = getElement('.draw');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const context = canvas.getContext('2d');
-context.strokeStyle = '#f06';
-context.lineJoin = 'round';
-context.lineCap = 'round';
-context.lineWidth = 100;
-
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
 let direction = true;
 
-const addEvent = (element, event, action) => element.addEventListener(event, action);
+const getElement = element => document.querySelector(element);
+const canvas = getElement('.draw');
+const context = canvas.getContext('2d');
+const rect = canvas.getBoundingClientRect();
 
-canvas.addEventListener('mousedown', () => {
-  isDrawing = true;
-  [lastX, lastY] = [event.offsetX, event.offsetY];
-});
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context.strokeStyle = '#f06';
+context.lineJoin = 'round';
+context.lineCap = 'round';
+context.lineWidth = 100;
+
+const addEvent = (element, event, action) => element.addEventListener(event, action);
 
 const draw = event => {
   if (!isDrawing) return;
@@ -36,18 +32,23 @@ const draw = event => {
 
   if (hue >= 360) {
     hue = 0;
-    context.lineWidth = 100;
   }
 
   if (context.lineWidth >= 100 || context.lineWidth <= 1) {
     direction = !direction;
   }
 
-  direction ? context.lineWidth++ : context.lineWidth;
+  direction ? context.lineWidth++ : context.lineWidth--;
 };
 
 const notDrawing = () => (isDrawing = false);
 
+const isMouseDown = () => {
+  isDrawing = true;
+  [lastX, lastY] = [event.offsetX, event.offsetY];
+};
+
 addEvent(canvas, 'mousemove', draw);
 addEvent(canvas, 'mouseup', notDrawing);
 addEvent(canvas, 'mouseout', notDrawing);
+addEvent(canvas, 'mousedown', isMouseDown);
