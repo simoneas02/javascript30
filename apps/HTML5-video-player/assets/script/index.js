f281a8422af6a6d295c15fa53c8e396d4ca77f4c
simoneas02 <simoneas03@gmail.com>
2019-06-25 11:12:21 -0300

    add function to update play and pause button

--- apps/HTML5-video-player/assets/script/index.js
+++ apps/HTML5-video-player/assets/script/index.js
@@ -1,8 +1,23 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
+const iconPlay = toggle.querySelector('.play');
+const iconPause = toggle.querySelector('.pause');
 
 const togglePlay = () => (video.paused ? video.play() : video.pause());
 
+const videoPlay = () => (
+  iconPause.classList.add('hide-button'),
+  iconPlay.classList.remove('hide-button')
+);
+
+const videoPause = () => (
+  iconPause.classList.remove('hide-button'),
+  iconPlay.classList.add('hide-button')
+);
+
+const updateButton = () => (video.paused ? videoPlay() : videoPause());
+
 video.addEventListener('click', togglePlay);
+video.addEventListener('click', updateButton);
 toggle.addEventListener('click', togglePlay);
