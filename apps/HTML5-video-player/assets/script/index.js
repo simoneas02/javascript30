e986b53e502ae71f91849b93a3dba1dc4a14acd5
simoneas02 <simoneas02@hotmail.com>
2019-06-25 16:29:25 -0300

    add function to forwards or backwards video

--- apps/HTML5-video-player/assets/script/index.js
+++ apps/HTML5-video-player/assets/script/index.js
@@ -3,21 +3,29 @@ const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
 const iconPlay = toggle.querySelector('.play');
 const iconPause = toggle.querySelector('.pause');
+const skipButtons = [...player.querySelectorAll('[data-skip]')];
+const playbackRate = player.querySelector('.playback__rate');
 
 const togglePlay = () => (video.paused ? video.play() : video.pause());
 
-const videoPlay = () => (
-  iconPause.classList.add('hide-button'),
-  iconPlay.classList.remove('hide-button')
-);
+const videoPlay = () => (iconPause.classList.add('hide-button'), iconPlay.classList.remove('hide-button'));
 
-const videoPause = () => (
-  iconPause.classList.remove('hide-button'),
-  iconPlay.classList.add('hide-button')
-);
+const videoPause = () => (iconPause.classList.remove('hide-button'), iconPlay.classList.add('hide-button'));
 
 const updateButton = () => (video.paused ? videoPlay() : videoPause());
 
+const skip = event => {
+	const currentTime = (video.currentTime += parseFloat(event.target.dataset.skip));
+	video.currentTime = currentTime;
+	playbackRate.value = currentTime;
+	playbackRate.max = video.duration;
+};
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('click', updateButton);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.map(button => button.addEventListener('click', skip));
