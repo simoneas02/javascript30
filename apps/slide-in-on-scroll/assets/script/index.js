8b71ba13b76674f716ce809fd3a89bcc0f04f30c
simoneas02 <simoneas02@hotmail.com>
2019-07-05 00:36:27 -0300

    add function to show and to hide the images with event scroll

--- apps/slide-in-on-scroll/assets/script/index.js
+++ apps/slide-in-on-scroll/assets/script/index.js
@@ -1,10 +1,28 @@
 'use strict';
 
-const checkSlide = () => {
+const addClasse = (element, style) => element.classList.add(style);
+const removeClasse = (element, style) => element.classList.remove(style);
+
+const showSliderImage = sliderImage => (
+  addClasse(sliderImage, 'slide-in--active'), removeClasse(sliderImage, 'slide-in--hide')
+);
+
+const hideSliderImage = sliderImage => (
+  removeClasse(sliderImage, 'slide-in--active'), addClasse(sliderImage, 'slide-in--hide')
+);
+
+const checkSlide = e => {
   const sliderImages = [...document.querySelectorAll('.slide-in')];
+
   sliderImages.map(sliderImage => {
-    console.log(`I'm scrolling`);
+    const { scrollY, innerHeight } = window;
+    const slideInAt = scrollY + innerHeight - sliderImage.height / 2;
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = scrollY < imageBottom;
+
+    isHalfShown && isNotScrolledPast ? showSliderImage(sliderImage) : hideSliderImage(sliderImage);
   });
 };
 
-window.addEventListener('scroll', checkSlide);
+window.addEventListener('scroll', checkSlide, true);
