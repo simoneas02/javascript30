93b7623e6803468df48b5d653174f6e248079d93
simoneas02 <simoneas02@hotmail.com>
2019-07-06 19:16:41 -0300

    add debounce function

--- apps/slide-in-on-scroll/assets/script/index.js
+++ apps/slide-in-on-scroll/assets/script/index.js
@@ -1,28 +1,42 @@
 'use strict';
 
-const addClasse = (element, style) => element.classList.add(style);
-const removeClasse = (element, style) => element.classList.remove(style);
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function() {
+    const context = this,
+      args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const addStyle = (element, style) => element.classList.add(style);
+const removeStyle = (element, style) => element.classList.remove(style);
 
 const showSliderImage = sliderImage => (
-  addClasse(sliderImage, 'slide-in--active'), removeClasse(sliderImage, 'slide-in--hide')
+  addStyle(sliderImage, 'slide-in--active'), removeStyle(sliderImage, 'slide-in--hide')
 );
 
 const hideSliderImage = sliderImage => (
-  removeClasse(sliderImage, 'slide-in--active'), addClasse(sliderImage, 'slide-in--hide')
+  removeStyle(sliderImage, 'slide-in--active'), addStyle(sliderImage, 'slide-in--hide')
 );
 
-const checkSlide = e => {
-  const sliderImages = [...document.querySelectorAll('.slide-in')];
+const sliderImages = [...document.querySelectorAll('.slide-in')];
 
+const checkSlide = () =>
   sliderImages.map(sliderImage => {
-    const { scrollY, innerHeight } = window;
+    const { scrollY, innerHeight } = this;
     const slideInAt = scrollY + innerHeight - sliderImage.height / 2;
     const imageBottom = sliderImage.offsetTop + sliderImage.height;
     const isHalfShown = slideInAt > sliderImage.offsetTop;
     const isNotScrolledPast = scrollY < imageBottom;
-
     isHalfShown && isNotScrolledPast ? showSliderImage(sliderImage) : hideSliderImage(sliderImage);
   });
-};
 
-window.addEventListener('scroll', checkSlide, true);
+window.addEventListener('scroll', debounce(checkSlide), true);
