aab8232563c98fce9caa0c2fb64fd72c09b22701
simoneas02 <simoneas02@hotmail.com>
2019-07-08 23:50:12 -0300

    add a gif to show how the application work

--- apps/slide-in-on-scroll/README.md
+++ apps/slide-in-on-scroll/README.md
@@ -1,7 +1,7 @@
 Slide In on Scroll
 ====================
 
-![]()
+![A gif with some text and Beyince pictures](./assets/img/queen.gif)
 
 > Vanilla JavaScript Slide In on Scroll - #JavaScript30 13/30
 
--- apps/slide-in-on-scroll/assets/script/index.js
+++ apps/slide-in-on-scroll/assets/script/index.js
@@ -3,12 +3,14 @@
 function debounce(func, wait = 20, immediate = true) {
   let timeout;
   return function() {
-    const context = this,
-      args = arguments;
+    const context = this;
+    const args = arguments;
+
     const later = function() {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
+
     const callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
