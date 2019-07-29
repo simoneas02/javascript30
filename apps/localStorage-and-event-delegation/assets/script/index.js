c9c682b3d7f721dd89d23bcc28035495f241f2df
simoneas02 <simoneas02@hotmail.com>
2019-07-28 22:39:26 -0300

    add local storage

--- apps/localStorage-and-event-delegation/assets/script/index.js
+++ apps/localStorage-and-event-delegation/assets/script/index.js
@@ -1,13 +1,17 @@
 const addItems = document.querySelector('.form');
 const itemsList = document.querySelector('.list');
+let itemsLocal = JSON.parse(localStorage.getItem('items')) || [];
 
 const addItem = e => {
   e.preventDefault();
   const text = getInputValue();
   const element = createElement(text);
 
+  itemsLocal = [...itemsLocal, text];
   itemsList.insertAdjacentHTML('beforeend', element);
 
+  // itemsList.insertAdjacentHTML('beforeend', useLocalStorage(itemsLocal));
+  localStorage.setItem('items', JSON.stringify(itemsLocal));
   e.target.reset();
 };
 
@@ -28,5 +32,8 @@ const createElement = item => `
           <label class="list__item__label" for="item">${item.text}</label>
         </li>
       `;
+
+const useLocalStorage = listLocalStorage =>
+  listLocalStorage.map(item => createElement(item)).join('');
 
 addItems.addEventListener('submit', addItem);
